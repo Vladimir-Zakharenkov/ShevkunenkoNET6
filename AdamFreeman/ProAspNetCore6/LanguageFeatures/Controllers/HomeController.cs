37688d6d883c371e279f012e76b003857f6c9d4c
Владимир Захаренков <zakharenkov1965@yandex.ru>
2022-11-27 17:55:41 +0300

    27-11-2022-17-55

---
+++ AdamFreeman/ProAspNetCore6/LanguageFeatures/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using LanguageFeatures.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LanguageFeatures.Controllers;
+
+public class HomeController : Controller
+{
+	public ViewResult Index()
+	{
+        //return View(new string[] { "C#", "Language", "Features" });
+
+        Product[] products = Product.GetProducts();
+        return View(new string[] { products[0].Name });
+
+    }
+}

684ea4ca7676b15a2cd42af9b96e9a3390f04c16
Владимир Захаренков <zakharenkov1965@yandex.ru>
2022-11-27 23:11:19 +0300

    27-11-2022-23-11

--- AdamFreeman/ProAspNetCore6/LanguageFeatures/Controllers/HomeController.cs
+++ AdamFreeman/ProAspNetCore6/LanguageFeatures/Controllers/HomeController.cs
@@ -47,7 +47,53 @@ public class HomeController : Controller
 
         #region Example4
 
-        return View(new string[] { products[0]!.Name });
+        //return View(new string[] { products[0]!.Name });
+
+        #endregion
+
+        #region Example5
+
+        //return View(new string[] {
+        //    $"Name: {products[0]?.Name}, Price: { products[0]?.Price }"
+        //    });
+
+        #endregion
+
+        #region Example6
+
+        //string[] names = new string[3];
+        //names[0] = "Bob";
+        //names[1] = "Joe";
+        //names[2] = "Alice";
+        //return View("Index", names);
+
+        #endregion
+
+        #region Example7
+
+        //return View("Index", new string[] { "Bob", "Joe", "Alice" });
+
+        #endregion
+
+        #region Example8
+
+        //Dictionary<string, Product> products2 = new Dictionary<string, Product> {
+        //        { "Kayak", new Product { Name = "Kayak", Price = 275M } },
+        //        { "Lifejacket", new Product{ Name = "Lifejacket", Price = 48.95M } }
+        //    };
+
+        //return View("Index", products2.Keys);
+
+        #endregion
+
+        #region Example9
+
+        Dictionary<string, Product> products2 = new Dictionary<string, Product>
+        {
+            ["Kayak"] = new Product { Name = "Kayak", Price = 275M },
+            ["Lifejacket"] = new Product { Name = "Lifejacket", Price = 48.95M }
+        };
+        return View("Index", products2.Keys);
 
         #endregion
     }

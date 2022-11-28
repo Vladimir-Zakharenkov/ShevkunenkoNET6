c68e7f889306c60d655700ae2e826b7f231e1762
Владимир Захаренков <zakharenkov1965@yandex.ru>
2022-11-28 20:57:21 +0300

    28-11-2022-20-57

--- AdamFreeman/ProAspNetCore6/LanguageFeatures/Controllers/HomeController.cs
+++ AdamFreeman/ProAspNetCore6/LanguageFeatures/Controllers/HomeController.cs
@@ -12,7 +12,7 @@ public class HomeController : Controller
 
     public ViewResult Index()
     {
-        Product?[] products = Product.GetProducts();
+        //Product?[] products = Product.GetProducts();
 
         #region Example1
 
@@ -253,32 +253,61 @@ public class HomeController : Controller
 
         #region Example18
 
-        ShoppingCart cart = new ShoppingCart { Products = Product.GetProducts() };
+        //ShoppingCart cart = new ShoppingCart { Products = Product.GetProducts() };
 
-        Product[] productArray = {
-                new Product {Name = "Kayak", Price = 275M},
-                new Product {Name = "Lifejacket", Price = 48.95M},
-                new Product {Name = "Soccer ball", Price = 19.50M},
-                new Product {Name = "Corner flag", Price = 34.95M}
+        //Product[] productArray = {
+        //        new Product {Name = "Kayak", Price = 275M},
+        //        new Product {Name = "Lifejacket", Price = 48.95M},
+        //        new Product {Name = "Soccer ball", Price = 19.50M},
+        //        new Product {Name = "Corner flag", Price = 34.95M}
+        //    };
+
+        ////Func<Product?, bool> nameFilter = delegate (Product? prod) {
+        ////	return prod?.Name?[0] == 'S';
+        ////};
+
+        //decimal priceFilterTotal = productArray
+        //    .Filter(p => (p?.Price ?? 0) >= 20)
+        //    .TotalPrices();
+
+        //decimal nameFilterTotal = productArray
+        //    .Filter(p => p?.Name?[0] == 'S')
+        //    .TotalPrices();
+
+        ////return View("Index", new string[] {
+        ////        $"Price Total: {priceFilterTotal:C2}",
+        ////        $"Name Total: {nameFilterTotal:C2}" });
+
+        ////return View(Product.GetProducts().Select(p => p?.Name));
+
+        //var names = new[] { "Kayak", "Lifejacket", "Soccer ball" };
+        //return View(names);
+
+        #endregion
+
+        #region Example19
+
+        //var products = new[] {
+        //        new { Name = "Kayak", Price = 275M },
+        //        new { Name = "Lifejacket", Price = 48.95M },
+        //        new { Name = "Soccer ball", Price = 19.50M },
+        //        new { Name = "Corner flag", Price = 34.95M }
+        //    };
+
+        //return View(products.Select(p => p.Name));
+
+        #endregion
+
+        #region Example20
+
+        var products = new[] {
+                new { Name = "Kayak", Price = 275M },
+                new { Name = "Lifejacket", Price = 48.95M },
+                new { Name = "Soccer ball", Price = 19.50M },
+                new { Name = "Corner flag", Price = 34.95M }
             };
 
-        //Func<Product?, bool> nameFilter = delegate (Product? prod) {
-        //	return prod?.Name?[0] == 'S';
-        //};
-
-        decimal priceFilterTotal = productArray
-            .Filter(p => (p?.Price ?? 0) >= 20)
-            .TotalPrices();
-
-        decimal nameFilterTotal = productArray
-            .Filter(p => p?.Name?[0] == 'S')
-            .TotalPrices();
-
-        //return View("Index", new string[] {
-        //        $"Price Total: {priceFilterTotal:C2}",
-        //        $"Name Total: {nameFilterTotal:C2}" });
-
-        return View(Product.GetProducts().Select(p => p?.Name));
+        return View(products.Select(p => p.GetType().Name));
 
         #endregion
     }
--- AdamFreeman/ProAspNetCore6/LanguageFeatures/Models/ShoppingCart.cs
+++ AdamFreeman/ProAspNetCore6/LanguageFeatures/Models/ShoppingCart.cs
@@ -12,14 +12,30 @@ using System.Collections;
 
 #region Example2
 
-public class ShoppingCart : IEnumerable<Product?>
+//public class ShoppingCart : IEnumerable<Product?>
+//{
+//    public IEnumerable<Product?>? Products { get; set; }
+
+//    public IEnumerator<Product?> GetEnumerator() => Products?.GetEnumerator()
+//        ?? Enumerable.Empty<Product?>().GetEnumerator();
+
+//    IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+//}
+
+#endregion
+
+#region Example3
+
+public class ShoppingCart : IProductSelection
 {
-    public IEnumerable<Product?>? Products { get; set; }
+    private List<Product> products = new();
 
-    public IEnumerator<Product?> GetEnumerator() => Products?.GetEnumerator()
-        ?? Enumerable.Empty<Product?>().GetEnumerator();
+    public ShoppingCart(params Product[] prods)
+    {
+        products.AddRange(prods);
+    }
 
-    IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    public IEnumerable<Product>? Products { get => products; }
 }
 
 #endregion

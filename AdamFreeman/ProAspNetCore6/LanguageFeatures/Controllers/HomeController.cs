a1e7a4709b32a0e55c167db550e8d0a1ad728515
Владимир Захаренков <zakharenkov1965@yandex.ru>
2022-11-28 09:45:09 +0300

    28-11-2022-09-45

--- AdamFreeman/ProAspNetCore6/LanguageFeatures/Controllers/HomeController.cs
+++ AdamFreeman/ProAspNetCore6/LanguageFeatures/Controllers/HomeController.cs
@@ -151,11 +151,30 @@ public class HomeController : Controller
 
         #region Example13
 
+        //ShoppingCart cart = new ShoppingCart { Products = Product.GetProducts() };
+
+        //decimal cartTotal = cart.TotalPrices();
+
+        //return View("Index", new string[] { $"Total: {cartTotal:C2}" });
+
+        #endregion
+
+        #region Example14
+
         ShoppingCart cart = new ShoppingCart { Products = Product.GetProducts() };
+
+        Product[] productArray = {
+                new Product {Name = "Kayak", Price = 275M},
+                new Product {Name = "Lifejacket", Price = 48.95M}
+            };
 
         decimal cartTotal = cart.TotalPrices();
 
-        return View("Index", new string[] { $"Total: {cartTotal:C2}" });
+        decimal arrayTotal = productArray.TotalPrices();
+
+        return View("Index", new string[] {
+            $"Cart Total: {cartTotal:C2}",
+            $"Array Total: {arrayTotal:C2}" });
 
         #endregion
     }

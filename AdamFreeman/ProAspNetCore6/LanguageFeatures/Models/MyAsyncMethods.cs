4a7320446ae02bccf1de6e3b468ba3ebe40c4182
Владимир Захаренков <zakharenkov1965@yandex.ru>
2022-11-28 22:09:06 +0300

    28-11-2022-22-08

---
+++ AdamFreeman/ProAspNetCore6/LanguageFeatures/Models/MyAsyncMethods.cs
@@ -0,0 +1,27 @@
+﻿namespace LanguageFeatures.Models;
+
+public class MyAsyncMethods
+{
+    #region Example1
+    //public static Task<long?> GetPageLength()
+    //{
+    //    HttpClient client = new HttpClient();
+    //    var httpTask = client.GetAsync("http://apress.com");
+    //    return httpTask.ContinueWith((Task<HttpResponseMessage> antecedent) =>
+    //    {
+    //        return antecedent.Result.Content.Headers.ContentLength;
+    //    });
+    //}
+    #endregion
+
+    #region Example2
+    public async static Task<long?> GetPageLength()
+    {
+        HttpClient client = new HttpClient();
+
+        var httpMessage = await client.GetAsync("http://apress.com");
+
+        return httpMessage.Content.Headers.ContentLength;
+    }
+    #endregion
+}

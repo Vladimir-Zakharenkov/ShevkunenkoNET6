6554d5b14837ff16eaa368f5ee97f6e13185f57f
Владимир Захаренков <zakharenkov1965@yandex.ru>
2022-12-02 23:47:57 +0300

    02-12-2022-23-47

--- AdamFreeman/ProAspNetCore6/SportsStore/Program.cs
+++ AdamFreeman/ProAspNetCore6/SportsStore/Program.cs
@@ -1,13 +1,18 @@
+using Microsoft.EntityFrameworkCore;
+using SportsStore.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
 
+builder.Services.AddDbContext<StoreDbContext>(opts =>
+{
+    opts.UseSqlServer(builder.Configuration["ConnectionStrings:SportsStoreConnection"]);
+});
+
 var app = builder.Build();
 
-//app.MapGet("/", () => "Hello World!");
-
 app.UseStaticFiles();
-
 app.MapDefaultControllerRoute();
 
 app.Run();

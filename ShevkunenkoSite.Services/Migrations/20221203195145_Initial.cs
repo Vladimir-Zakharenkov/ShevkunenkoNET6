050d07120c49baa49cda41fff223efcfac770657
Владимир Захаренков <zakharenkov1965@yandex.ru>
2022-12-03 23:01:43 +0300

    03-12-2022-23-01

---
+++ ShevkunenkoSite.Services/Migrations/20221203195145_Initial.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ShevkunenkoSite.Services.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "PageInfo",
+                columns: table => new
+                {
+                    PageInfoModelId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    PageTitle = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PageInfo", x => x.PageInfoModelId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PageInfo");
+        }
+    }
+}

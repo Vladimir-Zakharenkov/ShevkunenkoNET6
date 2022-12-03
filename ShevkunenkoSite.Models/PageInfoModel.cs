c0224f42ed750f778d8df9276c4ae5497da86ab0
Владимир Захаренков <zakharenkov1965@yandex.ru>
2022-12-03 20:27:10 +0300

    03-12-2022-20-27

---
+++ ShevkunenkoSite.Models/PageInfoModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ShevkunenkoSite.Models;
+
+public class PageInfoModel
+{
+    [Display(Name = " PageInfoId :")]
+    public Guid PageInfoModelId { get; set; }
+
+    [Required(ErrorMessage = "Добавьте заголовок страницы")]
+    [DataType(DataType.Text)]
+    [Display(Name = "Заголовок страницы :")]
+    public string PageTitle { get; set; } = string.Empty;
+}
---
+++ ShevkunenkoSite.Services/SiteDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ShevkunenkoSite.Models;
+
+namespace ShevkunenkoSite.Services;
+
+public class SiteDbContext : DbContext
+{
+    public SiteDbContext(DbContextOptions<SiteDbContext> options) : base(options) { }
+
+    public DbSet<PageInfoModel> PageInfo => Set<PageInfoModel>();
+}

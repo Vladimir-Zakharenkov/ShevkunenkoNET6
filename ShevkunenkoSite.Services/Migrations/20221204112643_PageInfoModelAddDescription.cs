273a34e8f26c741bff0eb2b10205092210243049
Владимир Захаренков <zakharenkov1965@yandex.ru>
2022-12-04 14:33:40 +0300

    04-12-2022-14-33

---
+++ ShevkunenkoSite.Services/Migrations/20221204112643_PageInfoModelAddDescription.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ShevkunenkoSite.Services.Migrations
+{
+    public partial class PageInfoModelAddDescription : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "PageDescription",
+                table: "PageInfo",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PageDescription",
+                table: "PageInfo");
+        }
+    }
+}
--- ShevkunenkoSite/SeedData.cs
+++ ShevkunenkoSite/SeedData.cs
@@ -1,6 +1,7 @@
-﻿using ShevkunenkoSite.Models;
+﻿namespace ShevkunenkoSite.Services;
 
-namespace ShevkunenkoSite.Services;
+//If you need to reset the database, then run this command in the ShevkunenkoSite.Services folder:
+//dotnet ef database --startup-project ..\ShevkunenkoSite  drop --force --context SiteDbContext
 
 public static class SeedData
 {
@@ -18,7 +19,15 @@ public static class SeedData
             context.PageInfo.AddRange(
             new PageInfoModel
             {
-                PageTitle = "Сайт, посвященный Сергею Шевкуненко",
+                PageTitle = "Сайт памяти Сергея Шевкуненко",
+                PageDescription = "Эта история по-своему уникальна и практически не имеет " +
+                    "аналогов в истории российского кинематографа. Подававший большие надежды " +
+                    "актер волею судьбы угодил в тюрьму и довольно быстро добился славы и " +
+                    "признания совсем в другой среде - уголовной. Последней ступенькой, " +
+                    "на которую сумел забраться в преступной иерархии этот бывший актер, " +
+                    "была должность «положенца», которая предшествует самому высокому " +
+                    "титулу в уголовной среде - вора в законе. Имя этого человека - " +
+                    "Сергей Шевкуненко.",
             }
             );
 

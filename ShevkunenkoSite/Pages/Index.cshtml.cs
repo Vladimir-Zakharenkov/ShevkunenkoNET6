bc46ab1b9b8fe7fc12a095c054cc5e840dc3c50a
Владимир Захаренков <zakharenkov1965@yandex.ru>
2022-12-10 11:47:54 +0300

    10-12-2022-11-47

--- ShevkunenkoSite/Pages/Index.cshtml.cs
+++ ShevkunenkoSite/Pages/Index.cshtml.cs
@@ -4,9 +4,6 @@ namespace ShevkunenkoSite.Pages
 {
     public class IndexModel : PageModel
     {
-        private LinkGenerator linkGenerator;
-        public IndexModel(LinkGenerator linkGenerator) => this.linkGenerator = linkGenerator;
-        public string PathByPage { get; set; }
 
         public void OnGet()
         {
@@ -14,9 +11,6 @@ namespace ShevkunenkoSite.Pages
 
             //var message2 = HttpContext.Request.Path.Value;
 
-            PathByPage = linkGenerator.GetPathByPage(HttpContext.Request.Path)!;
-
-            var wer = 1;
         }
     }
 }

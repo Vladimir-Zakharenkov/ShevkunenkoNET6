987950e2d7da57f59d6d17db3b37b552edfa1760
Владимир Захаренков <zakharenkov1965@yandex.ru>
2022-12-26 00:05:42 +0300

    26-12-2022-00-05

---
+++ ShevkunenkoSite/Pages/Shared/Components/Code/Menu.cs
@@ -0,0 +1,10 @@
+﻿namespace ShevkunenkoSite.Pages.Shared.Components.Code;
+
+[ViewComponent]
+public class Menu : ViewComponent
+{
+    public IViewComponentResult Invoke()
+    {
+        return View();
+    }
+}
--- ShevkunenkoSite/Program.cs
+++ ShevkunenkoSite/Program.cs
@@ -75,6 +75,8 @@ if (env.IsDevelopment())
     SeedData.EnsurePopulated(app);
 }
 
+app.UseStatusCodePagesWithReExecute("/Error{0}");
+
 // The default HSTS value is 30 days. You may want to change this
 // for production scenarios, see https://aka.ms/aspnetcore-hsts.
 app.UseHsts();

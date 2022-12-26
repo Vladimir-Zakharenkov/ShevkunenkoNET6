c3b113567c7015116fefac64fe25c6186421f89c
Владимир Захаренков <zakharenkov1965@yandex.ru>
2022-12-27 00:43:36 +0300

    27-12-2022-00-43

---
+++ MVC-SandBox/Controllers/ProductsController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Docs.Samples;
+
+namespace MVC_SandBox.Controllers
+{
+    public class ProductsController : Controller
+    {
+        public IActionResult Details(int id)
+        {
+            return ControllerContext.MyDisplayRouteInfo(id);
+        }
+    }
+}
